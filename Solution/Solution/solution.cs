2a599276093b285d2ca63d652c06010f7f335feb
BC226H2-OOBE-01\venkat <suganyapselvam@gmail.com>
2020-12-15 11:16:10 +0530

    i created solution file

---
+++ Solution/Solution/solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution
+{
+    class solution
+    {
+        public bool Isprime(int num)
+        {
+            return true;
+        }
+        public bool Isfibisonsi(int num)
+        {
+            return true;
+        }
+        public bool Palindrome(int num)
+        {
+            return true;
+        }
+        public string ToBinary(int num)
+        {
+            return "a";
+        }
+        public string Multiplystr(string str,int num)
+        {
+            return "A";
+        }
+    }
+}
